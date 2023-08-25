61eebd5928ac60e114ce582e423fc19c97287e07
ZtRXR <Zengtudor@outlook.com>
2023-08-25 21:35:24 +0800

    更改密码

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,7 +12,7 @@ export const AppDataSource = new DataSource({
     host: "localhost",
     port: 3306,
     username: "root",
-    password: "Zengtudor",
+    password: "ZengtudorRXR2008",
     database: "hot",
     synchronize: true,
     logging: false,
